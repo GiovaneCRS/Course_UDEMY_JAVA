c632554e82104f5b430283428d0afc73bc1bd60a
GiovaneCRS <giovanecorrea2003@gmail.com>
2026-05-11 16:20:06 -0300

    change 5

---
+++ DrawbackArray.java
@@ -0,0 +1,20 @@
+public class DrawbackArray {
+    public static void main(String[] args)
+    {
+        int nums[] = new int [6];
+        
+        nums[0] = 4;
+        nums[1] = 2;
+        nums[2] = 6;
+        nums[3] = 1;
+        nums[4] = 8;
+        nums[5] = 4;
+
+
+        for(int i=0;i<=6; i++)
+            {
+                System.out.println(nums[i]);
+            }
+            System.out.println();
+    }
+}
--- MultiArray.java
+++ MultiArray.java
@@ -2,6 +2,21 @@ public class MultiArray
 {
     public static void main(String[] args)
     {
+        int nums[][] = new int [3][];
         
-    }    
+        nums[0] = new int[2];
+        nums[1] = new int[5];
+        nums[2] = new int[3];
+       
+       
+        for(int i=0; i<nums.length; i++) 
+        {
+            for(int j=0; j<nums[i].length; j++)
+                {
+                    nums[i][j] = (int)(Math.random()*10); 
+                    System.out.println(nums[i][j] + " ");
+                }
+                System.out.println();
+        }    
+    }
 }
